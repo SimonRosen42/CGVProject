f998868ededa5af8588eec3bbe94fd135201b49e
zaeemasvat <z.asvat07@gmail.com>
2019-04-09 13:19:13 +0200

    updated enemyHandler

--- TwinStickShooter/TwinStickShooter/js/game/game.levelHandler.js
+++ TwinStickShooter/TwinStickShooter/js/game/game.levelHandler.js
@@ -1,12 +1,44 @@
 window.game = window.game || {}
 
+import { Pathfinding } from 'three-pathfinding';
+
 window.game.levelHandler = function () {
+
 		create: function() {
 		// Create a solid material for all objects in the world
 		window.game.core._cannon.solidMaterial = window.game.core._cannon.createPhysicsMaterial(new CANNON.Material("solidMaterial"), 0, 0.1);
 		// Define floor settings
-		var floorSize = 800;
+		var floorSize = 800.0;
 		var floorHeight = 20;
+
+		var geometry = new THREE.BufferGeometry();
+
+		var vertices = new Float32Array ( [
+
+					-floorSize/2, -floorSize/2, 0.0,
+					floorSize/2, -floorSize/2, 0.0,
+					-floorSize/2, floorSize/2, 0.0,
+
+					-floorSize/2, floorSize/2, 0.0,
+					floorSize/2, -floorSize/2, 0.0,
+					floorSize/2, floorSize/2, 0.0					
+
+				]);
+
+		// add vertices to our geometry
+		geometry.addAttribute ('position', new THREE.BufferAttribute(vertices, 3));
+
+		// create level
+		const pathfinding = new Pathfinding();
+		const zone = 'level1';
+		pathfinding.setZoneData(zone, Pathfinding.createZone(geometry));
+		
+		findPath = function(a, b) {
+			const groupID = pathfinding.getGroup(zone, a);
+			const path = pathfinding.findPath(a, b, zone, groupID);
+			return path;
+		}
+
 		// Add a floor
 		window.game.core._cannon.createRigidBody({
 			shape: new CANNON.Box(new CANNON.Vec3(floorSize, floorSize, floorHeight)),
