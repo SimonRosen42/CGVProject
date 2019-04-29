40e350883ebdcd53880a88a7ba362819af2101f8
SimonRosen42 <simonrosen42@gmail.com>
2019-04-29 19:11:21 +0200

    Basic Changes

--- TwinStickShooter/TwinStickShooter/js/game/game.core.js
+++ TwinStickShooter/TwinStickShooter/js/game/game.core.js
@@ -16,7 +16,7 @@ window.game.core = function () {
 			//_game.level.create();
 			//_game.enemy.create();
 			// Initiate the game loop
-			//_game.loop();
+			_game.loop();
 		},
 		destroy: function() {
 			// Pause animation frame loop
@@ -24,34 +24,32 @@ window.game.core = function () {
 
 			// Destroy THREE.js scene and Cannon.js world and recreate them
 			_cannon.destroy();
-			_three.reset(_cannon);
-			//delete all enemies and players
+			_cannon.setup();
+			//_three.destroy();
+			//_three.setup();
 			_playerHandler.destroy();
 			_enemyHandler.destroy();
+
+			// Create level again
+			//_game.level.create();
+
+			// Continue with the game loop
+			_game.loop();
 		},
 		loop: function() {
+			
 			var dt = _clock.getDelta();
+
 			// Assign an id to the animation frame loop
 			_animationFrameLoop =  window.requestAnimationFrame(_game.loop);
 			_controllerHandler.updateStatus();
 			// Update Cannon.js world and player state
 			_cannon.updatePhysics();
-			_playerHandler.updatePlayers(dt);
+			_playerHandler.updatePlayers();
 			//_game.enemy.update();
 			_enemyHandler.updateEnemies(dt);
 			// Render visual scene
 			_three.render();
-		},
-		reset: function() {
-			_game.destroy();
-			_game.start();
-			_enemyHandler.addEnemy(new THREE.Vector3(5,2,5));
-		},
-		start: function() {
-			_clock = new THREE.Clock(true);
-			_clock.start();
-
-			_game.loop();
 		},
 		initComponents: function (options) {
 			// Reference game components one time
@@ -62,16 +60,20 @@ window.game.core = function () {
 			_controllerHandler = window.game.controllerHandler();
 			_enemyHandler = window.game.enemyHandler();
 			_playerHandler = window.game.playerHandler();
-			_options = options;
+			_levelHandler = window.game.levelHandler(); //my new code
+
 			// Initialize components with options
+			_cannon.init(_three);
+			_three.init(_cannon, options);
+			_ui.init();
+			_events.init();
+
+			_clock = new THREE.Clock(true);
+			_clock.start();
+			
 			_controllerHandler.init(_playerHandler);
 			_playerHandler.init(_cannon,_three,_game,_controllerHandler,_ui,_enemyHandler);
 			_enemyHandler.init(_cannon,_three,_game,_playerHandler);
-			_events.init();
-			_ui.init();
-			_cannon.init(_three);
-			_three.init(_cannon, _options);
-			_game.start(options);
 			for (var i = 0; i < 7; i++) {
 				var xP = (Math.random()-0.5)*20;
           	    var yP = 1 + (Math.random()-0.5)*1;
@@ -84,8 +86,7 @@ window.game.core = function () {
 			_events.onKeyDown = function () {
 				if (!_ui.hasClass("infoboxIntro", "fade-out")) {
 					_ui.fadeOut("infoboxIntro");
-					if (!_events.keyboard.pressed["w"])
-						_playerHandler.addPlayer();
+					_playerHandler.addPlayer();
 				}
 				if (_events.keyboard.pressed["leftArrow"]) {
 					_three.camera.position.set(_three.camera.position.x+0.1,_three.camera.position.y,_three.camera.position.z);
@@ -102,9 +103,6 @@ window.game.core = function () {
 				if (_events.keyboard.pressed["downArrow"]) {
 					_three.camera.position.set(_three.camera.position.x,_three.camera.position.y,_three.camera.position.z-0.1);
 					_three.camera.lookAt(0,0,0);
-				}
-				if (_events.keyboard.pressed["w"]) {
-					_game.reset();
 				}
 			};
 
@@ -125,7 +123,8 @@ window.game.core = function () {
 	var _animationFrameLoop;
 	var _enemyHandler;
 	var _playerHandler;
-	var _options;
+	var _levelHandler;
+
 	var _clock;
 
 	return _game;
